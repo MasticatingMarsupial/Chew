7ba4db37be9660f6ed207af79c71a21f7a3ab967
Michael Junio <michael.ray.junio@gmail.com>
2015-10-14 16:15:20 -0700

    Added django models for db

--- server/Chew/settings.py
+++ server/Chew/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Chew.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycog2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': 'port'
     }
 }
 
--- server/food/models.py
+++ server/food/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=30)
+
+class Review(models.Model):
+  text = models.CharField(max_length=255)
+  user = model.ForeignKey(User_id) # id only
+  foodRating = models.IntegerField()
+  reviewRating = models.IntegerField()
+  food = model.ForeignKey(Food_id) # id only 
+
+class Image(model.Model):
+  food = model.ForeignKey(Food_id) # id only
+  image = model.CharField(max_length=255)
+  review = model.ForeignKey(Review)
+
+class Food(models.Model):
+  name = model.CharField(max_length=255)
+  cuisine = model.ForeignKey(Cuisine_id) # id only
+  restaurant = model.ForeignKey(Restaurant)
+  price = model.IntegerField()
+  avgRating = model.IntegerField()
+  numRating = model.IntegerField()
+
+class Restaurant(model.Model):
+  name = model.CharField(max_length=255)
+  location = model.CharField(max_length=255)
+  cuisine = model.ForeignKey(Cuisine_id) # id only
+
+class Cuisine(model.Model):
+  name = model.CharField(max_length=255)
+
+class FoodTag(model.Model):
+  food = model.ForeignKey(Food_id) # id only
+  tag = model.ForeignKey(Tag_id) # id only
+
+class Tag(model.Model):
+  name = model.CharField(max_length=255)
