3880d557f7bc0fc8bbb2a062f37502b275f29f2d
Alex Manusu <alex.manusu@gmail.com>
2015-10-16 21:10:07 -0700

    Small fix to FoodSearchResultView and added intial code to android index

--- ChewApp/index.android.js
+++ ChewApp/index.android.js
@@ -7,14 +7,75 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  BackAndroid,
+  Navigator,
   StyleSheet,
+  ToolbarAndroid,
   Text,
   View,
 } = React;
 
+var HomeView = require('./js/components/HomeView');
+var SearchBar = require('./js/components/HomeView');
+
+//Keeps track of which page we are on
+var _navigator;
+
+//Checks to see if we can use the Android Back Button
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+    _navigator.pop();
+    return true;
+  }
+  return false;
+});
+
+var RouteMapper = function(route, navigationOperations, onComponentRef) {
+  _navigator = navigationOperations;
+  console.log("RouteMapper")
+  console.log("Route is:",route);
+ if (!route) {
+    return (
+      <HomeView navigator={navigationOperations} />
+    );
+  } 
+// TODO: Rework to handle the food pages
+  // else if (route && route.name === 'food') {
+  //   return (
+  //     <View style={{flex: 1}}>
+  //       <ToolbarAndroid
+  //         actions={[]}
+  //         navIcon={{uri: 'https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_arrow_back_48px-128.png'}}
+  //         onIconClicked={navigationOperations.pop}
+  //         style={styles.toolbar}
+  //         titleColor="white"
+  //         title='Cool title' />
+  //       <MovieScreen
+  //         style={{flex: 1}}
+  //         navigator={navigationOperations}
+  //         movie={route.movie}
+  //       />
+  //     </View>
+  //   );
+  // }
+};
+
+
 var ChewApp = React.createClass({
   render: function() {
+    var initialRoute = {name: 'home'};
     return (
+      <View>
+      <Navigator
+        style={styles.navigator}
+        initialRoute={initialRoute}
+        configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+        renderScene={RouteMapper}
+        />
+        <SearchBar
+          onSearchChange={this.onSearchChange}
+          isLoading={this.state.isLoading}
+        />
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -26,11 +87,15 @@ var ChewApp = React.createClass({
           Shake or press menu button for dev menu
         </Text>
       </View>
+      </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  navigator: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
