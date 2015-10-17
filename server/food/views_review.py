f272c801eb48d05c2747246c4db91600e6e57341
Michael Junio <michael.ray.junio@gmail.com>
2015-10-16 17:21:33 -0700

    Removed some excess code from merge; removed instances of FoodTag.

--- server/food/views_review.py
+++ server/food/views_review.py
@@ -2,21 +2,8 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from food.models import *
-from food.serializers import FoodSerializer, ReviewSerializer, ImageSerializer
+from food.serializers_review import ReviewSerializer
 from django.http import Http404
-
-class FoodList(APIView):
-  def get(self, request, format=None):
-    foods = Food.objects.all()
-    serializer = FoodSerializer(foods, many=True)
-    return Response(serializer.data)
-
-  def post(self, request, format=None):
-    serializer = FoodSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ReviewList(APIView):
   def get(self, request, format=None):
@@ -56,46 +43,6 @@ class ReviewDetail(APIView):
     review = self.get_object(pk)
     review.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
-
-class Imagelist(APIView):
-  def get(self, request, format=None):
-    image = Image.objects.all()
-    serializer = ImageSerializer(images, many=True)
-    return Response(serializer.data)
-
-  def post(self, request, format=None):
-    serializer = ImageSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-class ImageDetail(APIView):
-  def get_object(self, pk):
-    try:
-      return Image.objects.get(pk=pk)
-    except Image.DoesNotExist:
-      raise Http404
-
-  def get(self, request, format=None):
-    images = self.get_object(pk)
-    serializer = ImageSerializer(images)
-    return Response(serializer.data)
-
-  def put(self, request, format=None):
-    image = self.get_object(pk)
-    serializer = ImageSerializer(image, data=request.data)
-    if serializer.is_valpk():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-  def delete(self, request, pk, format=None):
-    image = self.get_object(pk)
-    image.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 
 
 
