613f0db1b91914eddda1e8c279419d1387e66286
Gary Ye <garysye@gmail.com>
2015-11-06 11:08:48 -0800

    Changes review serializers to use username

--- server/food/serializers_review.py
+++ server/food/serializers_review.py
@@ -3,23 +3,13 @@ from food.models import Review, Food
 from food.serializers_food import FoodSerializer
 from django.contrib.auth.models import User
 
-class CreateableSlugRelatedField(serializers.SlugRelatedField):
-  def to_internal_value(self, data):
-    try:
-      return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
-    except ObjectDoesNotExist:
-      self.fail('does_not_exist', slug_name=self.slug_field, value=smart_text(data))
-    except (TypeError, ValueError):
-      self.fail('invalid')
-
 class UserSerializer(serializers.ModelSerializer):
-  reviews = serializers.PrimaryKeyRelatedField(many=True, queryset=Review.objects.all())
   class Meta:
     model = User
-    fields = ['id', 'username', 'reviews']
+    fields = ['username', 'first_name', 'last_name']
 
 class ReviewSerializer(serializers.ModelSerializer):
-  owner = serializers.ReadOnlyField(source='owner.username')
+  owner = UserSerializer(read_only=True)
 
   class Meta:
     model = Review
