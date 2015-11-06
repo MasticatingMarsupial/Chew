2e916b6bca2b91d8313c5129d8ed2a566b485c13
Michael Junio <michael.ray.junio@gmail.com>
2015-11-05 20:49:41 -0800

    Localhosts

--- ChewApp/js/actions/UserActions.js
+++ ChewApp/js/actions/UserActions.js
@@ -3,8 +3,8 @@
 var AppDispatcher = require('../dispatchers/AppDispatcher');
 var UserConstants = require('../constants/UserConstants');
 
-// var API_URL = 'http://chewmast.herokuapp.com/api/';
-var API_URL = 'http://localhost:8000/api/';
+var API_URL = 'http://chewmast.herokuapp.com/api/';
+// var API_URL = 'http://localhost:8000/api/';
 
 var UserActions = {
 
